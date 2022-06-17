01dac5364726b7aaa1089f408d995dddec9048b0
kazlo <gan-erdene@tanusoft.mn>
2022-06-17 14:09:10 +0800

    1

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
