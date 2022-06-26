e38a00a8057ecab6c79996d8619dcb0aa254c85f
kazlo1290 <kazlo1290@gmail.com>
2022-06-26 18:19:43 +0800

    `build` test

--- client/pre/src/App.js
+++ client/pre/src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code></code> Дараах сайт луу явна уу!!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="https://colorfully.mn"
           rel="noopener noreferrer"
         >
-          Learn React
+          colorfully.mn
         </a>
-      </header>
+      </div>
     </div>
   );
 }
